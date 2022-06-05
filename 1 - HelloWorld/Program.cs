f4ab0afc8fee0558280e3e81cacf5bc1ec333d5a
Allen Kemp <allenkemp@gmail.com>
2022-06-04 22:52:35 -0500

    Added Tutorial 1 - 3

--- 1 - HelloWorld/Program.cs
+++ 1 - HelloWorld/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Using Statements
+    Where we include the modules and other source code objects we
+    want to be able to reference things from in our program.
+    "using System;" is almost always going to be here if nothing else when 
+    we are working in dotnet
+*/
+using System;
+
+/*
+    Namespace Declaration
+
+    Namespaces keep our code from colliding with the code of others
+    As a convention, I have my namespace match my program name, here "HelloWorld"
+    For namespaces, i use capitallized Pascal case (first letter of each word capitalized, no underscores)
+*/
+namespace HelloWorld
+{
+    /*
+        Program Class
+        the class that contains our Program and its main method.
+    */
+
+    internal class Program
+    {
+        /* 
+            Main Method
+
+            The entrypoint into our program.  We must have a Main method for our program to work properly
+
+            The [] args are string array of command line arguments.
+            Note that Main is a void function, it doesn't return anything.
+        */
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); //  The only line of our program, Just outputs 'old faithful' out
+                                                //  to the terminal.
+                                                //  I put these comments here just to show you they can pretty much
+                                                //  go anywhere we want.
+        }
+    }    
+}
---
+++ 2 - InputOutput/Program.cs
@@ -0,0 +1,24 @@
+﻿// In this example, i'm taking advantage of the "new" top-level statements 
+// functionality of dotnet.  Here, dotnet will handle all of the ceremonial boilerplate we need to make our
+// program work.  We don't have to worry about the usings, namespace, class, etc... 
+
+// In this example program, lets ask the user a couple of questions and get back some responses from 
+// the command line.  We will use this to learn how to do some very simple I/O.
+
+// First, let's prompt the user for some input
+Console.WriteLine("Hello, Welcome to our awesome program.\nWhat is your Name?");
+
+// Next, let's grab our user's response from the console.
+// We declare a local string variable 'userName' to hold our user's input.  
+// Note, for local variables, we want to use camel Case, that is, the first word
+// is lowercase and each subsequent word in our variable name is capitalized.
+
+string userName = Console.ReadLine();
+
+// Now, let's echo our user's name back to them in an attempt to impress them.
+// Here, we've used a different way of expressing a string.  By putting the '@' before our string, 
+// we are making a 'verbatim' string... our whitespace and new line are respected.
+// I did it here to show you an example and to make my line a little easier to read.
+Console.WriteLine(@"Hello, {0}! So glad you could join us. 
+These little programs are dumb, but sometimes it do be like that.", userName);
+
